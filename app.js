5cbe8466a82e82edbef168f47dc2507812860094
Umer qazi <uqazi844@gmail.com>
2024-04-20 22:41:49 +0500

    good

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
-}
-
-console.log(process.env.secret);
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
